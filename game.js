209c80f066eb5196c1c4b792b915452487ff83b0
Pete Sevander <pete.sevander@gmail.com>
2014-10-24 22:51:23 +0300

    pyörivä pallo testailua ja funktioita valmiiksi

--- game.js
+++ game.js
@@ -1,11 +1,39 @@
-var stage = new createjs.Stage("gameCanvas");
+var stage, ball, crosshair;
+var rot = false;
 
-var ball = new createjs.Bitmap("ball.png");
-stage.addChild(ball);
+function init() {
+    stage = new createjs.Stage("gameCanvas");
 
-createjs.Ticker.addEventListener("tick", handleTick);
+    crosshair = new createjs.Bitmap("ball.png");
+    crosshair.regX = 32;
+    crosshair.regY = 32;
+    stage.addChild(crosshair);
 
+    createjs.Ticker.addEventListener("tick", handleTick);
+    createjs.Ticker.setFPS(60);
+    stage.on('stagemousedown', handleMouseDown);
+    stage.on('stagemouseup', handleMouseUp);
+    stage.on('stagemousemove', handleMouseMove);
+
+}
 function handleTick(event) {
-    ball.x += 5;
+    if (rot) {
+        crosshair.rotation += event.delta/1000 * 100;
+    }
     stage.update();
 }
+
+function handleMouseUp(event) {
+    // stop shooting
+    rot = false;
+}
+function handleMouseDown(event) {
+    // start shooting
+    rot = true;
+}
+function handleMouseMove(event) {
+    crosshair.x = event.stageX;
+    crosshair.y = event.stageY;
+}
+
+init();
