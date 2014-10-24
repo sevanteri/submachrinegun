331be4b0c52d0cd201f21f3b071164f7b6a2a6d2
Pete Sevander <pete.sevander@gmail.com>
2014-10-25 00:03:52 +0300

    'player' moves away from crosshair when shooting

--- game.js
+++ game.js
@@ -1,8 +1,4 @@
 var stage, player, crosshair;
-var rot = false;
-
-var playerSpeed = [0,0];
-var playerMaxSpeed = [3, 2];
 
 var KEYCODE_W = 87;
 var KEYCODE_A = 65;
@@ -13,9 +9,15 @@ function init() {
     stage = new createjs.Stage("gameCanvas");
 
     player = new createjs.Bitmap("ball.png");
-    crosshair = new createjs.Bitmap("ball.png");
-    crosshair.regX = 32;
-    crosshair.regY = 32;
+    player.speed = [0,0];
+    player.x = stage.canvas.width/2;
+    player.y = stage.canvas.height/2;
+    player.regX = 32;
+    player.regY = 32;
+
+    crosshair = new createjs.Bitmap("crosshair.png");
+    crosshair.regX = 25;
+    crosshair.regY = 25;
 
     stage.addChild(player);
     stage.addChild(crosshair);
@@ -30,27 +32,39 @@ function init() {
     document.onkeyup = handleKeyUp;
 }
 function handleTick(event) {
-    if (rot) {
-        crosshair.rotation += event.delta/1000 * 200;
-    }
     // move player
-    player.x += event.delta/1000*playerSpeed[0]*100;
-    player.y += event.delta/1000*playerSpeed[1]*100;
+    player.x += event.delta/1000*player.speed[0]*10;
+    player.y += event.delta/1000*player.speed[1]*10;
+
+    player.speed[0] /= 1.112;
+    player.speed[1] /= 1.112;
     stage.update();
 }
 
 function handleMouseUp(event) {
     // stop shooting
-    rot = false;
 }
 function handleMouseDown(event) {
     // start shooting
-    rot = true;
+
+    // calculate direction between player and crosshair
+    var mToPX = event.stageX - player.x;
+    var mToPY = event.stageY - player.y;
+
+    var l = Math.sqrt(Math.pow(mToPX, 2) + Math.pow(mToPY, 2));
+
+    var dirX = mToPX / Math.abs(l);
+    var dirY = mToPY / Math.abs(l);
+
+    console.log(dirX, dirY);
+    player.speed[0] += -dirX*50;
+    player.speed[1] += -dirY*50;
 }
 function handleMouseMove(event) {
     crosshair.x = event.stageX;
     crosshair.y = event.stageY;
 }
+
 function handleKeyDown(event) {
     if (!event) event = window.event;
     //console.log(event.keyCode);
