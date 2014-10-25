f464c9b3856fab7629fb5c532bdd4fccd5ca4e74
pottumuusi <backman-93@hotmail.com>
2014-10-25 20:29:33 +0300

    added makeBoundswSize to walls_js

--- walls.js
+++ walls.js
@@ -34,6 +34,38 @@ function makeBounds(){
     stage.addChild(walls[3]);
 }
 
+function makeBoundswSize(size){
+    var i = 0;
+    while (i < 4){
+        walls[i] = new createjs.Shape();
+        i++;
+    }
+    walls[0].graphics.beginFill("blue").drawRect(0,0,stage.canvas.width,size);
+    walls[1].graphics.beginFill("blue").drawRect(0,0,stage.canvas.width,size);
+    walls[2].graphics.beginFill("blue").drawRect(0,0,size,stage.canvas.height);
+    walls[3].graphics.beginFill("blue").drawRect(0,0,size,stage.canvas.height);
+  
+    // walls[0] is roof
+    walls[0].x = 0;
+    walls[0].y = 0;
+    stage.addChild(walls[0]);
+    
+    // rectangle1 is floor
+    walls[1].x = 0;
+    walls[1].y = stage.canvas.height - size;
+    stage.addChild(walls[1]);
+
+    // rectangle2 is left wall
+    walls[2].x = 0;
+    walls[2].y = 0;
+    stage.addChild(walls[2]);
+
+    // rectangle3 is right wall
+    walls[3].x = stage.canvas.width - size;
+    walls[3].y = 0;
+    stage.addChild(walls[3]);
+}
+
 function verticalObstacle(x, y, length){
     var i = walls.length;
     
